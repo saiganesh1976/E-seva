0055631cb0e19c47fe3c50aff880252421970f37
saiganesh1976 <saiganeshratnala01@gmail.com>
2025-03-13 11:17:00 +0530

    Fixed authController.js import issue

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,4 +1,4 @@
-import userModel from "../models/userModel.js";
+import userModel from "../Models/userModel.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import validator from "validator";
